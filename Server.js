6235c059fee10a1f7bd73265c97cf2b7dc724d63
Ch. Krishna Aasritha <2200031767@kluniversity.in>
2024-05-01 03:36:03 +0530

    Changed Server.js file

--- Server.js
+++ Server.js
@@ -1,11 +1,22 @@
 const express= require("express")
 const mongoogse = require("mongoose")
 const cors=require("cors")
-const dburl = "mongodb://localhost:27017/sdpproject32"
+// const dburl = "mongodb://localhost:27017/sdpproject32"
 
-mongoogse.connect(dburl).then(() => {
-    console.log("Connected To DB Successfully")    
-}).catch((err) => {
+// mongoogse.connect(dburl).then(() => {
+//     console.log("Connected To DB Successfully")    
+// }).catch((err) => {
+//     console.log(err.message)
+// });
+
+const dburl ="mongodb+srv://admin:admin@cluster0.8iizv1c.mongodb.net/sdpproject32?retryWrites=true&w=majority&appName=Cluster0"
+//connection step
+//db - demoproject32
+mongoogse.connect(dburl).then(()=>
+{
+    console.log("Connected to MongoDB Atlas Successfully")
+}).catch((err)=>
+{
     console.log(err.message)
 });
 
@@ -15,11 +26,10 @@ app.use(cors())
 
 const  adminrouter = require("./routes/adminroutes")
 const customerrouter = require("./routes/customerroutes")
- const recommendrouter = require('./routes/recommendroutes');
+ const router = require('./routes/recommendroutes');
  const sellerrouter = require('./routes/sellerroutes');
 
- app.use("", recommendrouter);
-
+ app.use("", router);
 app.use("",adminrouter)
 app.use("",customerrouter) 
 app.use('', sellerrouter);
